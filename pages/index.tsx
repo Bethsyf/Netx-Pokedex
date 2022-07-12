836c58fb3736945be184a8b0a23d64b6430df0da
Bethsyf <bethsyfalcon@gmail.com>
2022-07-12 09:14:38 -0500

    show data ApiPokemon {id, name}

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,52 @@
 import type { NextPage } from 'next'
 import { Layout } from '../components/layouts';
+import { GetStaticProps } from 'next'
+import pokeApi from '../api/pokeApi';
+import { PokemonlistResponse, SmallPokemon } from '../interfaces/pokemon-list';
 
-const HomePage: NextPage = () => {
-  return (    
+interface Props{
+  pokemons: SmallPokemon[];
+  
+}
+
+const HomePage: NextPage<Props> = ( {pokemons} ) => {
+  console.log(pokemons) 
+  return (   
+    
     <Layout title='Listado de Pokémons'>
-      <h1>Hola Mundo</h1>
+      <ul>
+        {
+        pokemons.map( ({id, name, img}) => (
+          <li key={ id }>
+            #{ id } - { name } 
+            
+          </li>
+        )      
+        )
+        }
+      </ul>
     </Layout>   
   )
+}
+
+
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+ 
+  const { data } = await pokeApi.get<PokemonlistResponse>('/pokemon?limit=151')
+  console.log(data)
+  const pokemons: SmallPokemon[] = data.results.map( (poke, index ) => ({
+    ...poke, 
+    id: index + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ index + 1 }`
+  }))
+
+  return {
+    props: {
+      pokemons
+    }
+  }
 }
 
 export default HomePage
