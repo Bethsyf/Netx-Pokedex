6106388f63d1884bd71b12cdd82ba7ae31220ca4
Bethsyf <bethsyfalcon@gmail.com>
2022-07-13 14:35:07 -0500

    styles PokemonCard

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -4,27 +4,28 @@ import { SmallPokemon } from '../../interfaces/pokemon-list';
 import styles from './PokemonCard.module.scss'
 
 
-interface Props {  
+interface Props {
   pokemon: SmallPokemon
 }
 
-export const PokemonCard: FC<Props> = ({pokemon}) => {
+export const PokemonCard: FC<Props> = ({ pokemon }) => {
   return (
-    
-      <div className={ styles.card} key={ pokemon.id }>
-            <Image 
-              src={pokemon.img} 
-              alt={pokemon.name}
-               width={150} 
-               height={150} 
-               placeholder='blur'
-               blurDataURL={pokemon.img}
-            />
 
-            <h3>{ pokemon.name }</h3>
-            <h3>#{ pokemon.id }</h3>
-          </div>
-    
+    <div className={styles.card} key={pokemon.id}>
+      <Image
+        src={pokemon.img}
+        alt={pokemon.name}
+        width={150}
+        height={150}
+        placeholder='blur'
+        blurDataURL={pokemon.img}
+      />
+      <div className={styles.title}>
+        <h3>{pokemon.name}</h3>
+        <h3>#{pokemon.id}</h3>
+      </div>
+    </div>
+
   )
 }
 
