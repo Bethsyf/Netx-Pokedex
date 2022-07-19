84948220cc463ecf801032af54fd76a94a5efd32
Bethsyf <bethsyfalcon@gmail.com>
2022-07-19 13:55:59 -0500

    autoprefixer

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -3,14 +3,25 @@ import { Pokemon } from "../interfaces/pokemon-full";
 
 export const getPokemonInfo = async( id: string ) => {
 
+    try{
   
     // const { data } = await pokeApi.get<Pokemon>(`/pokemon/${ nameOrId }`);
     const { data } = await pokeApi.get<Pokemon>(`/pokemon/${ id }`)
 
+    // if ( !pokemon ) {
+    //     return {
+    //     redirect: {
+    //         destination: '/',
+    //         permanent: false
+    //     }
+    // }
+    // }
     return {
         id: data.id,
         name: data.name,
         sprites: data.sprites
     }
-
+} catch (error) {
+    return null;
+}
 }
